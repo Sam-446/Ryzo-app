d4d67118362e1a97cb60f091705457f2c78b6164
Sam-446 <ismailrajput399@gmail.com>
2026-05-12 04:09:28 +0500

    Unregister old service workers

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// Unregister any old service workers from previous builds
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((reg) => reg.unregister());
+  });
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
